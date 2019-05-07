c34d5b7142513b81f61790e771f8fe78a1dd7400
yan <809122235@qq.com>
2019-05-07 11:17:09 +0800

    TextBoxWithRule
    TextBoxWithRule

---
+++ Module.Controls/TextBoxWithRule.xaml.cs
@@ -0,0 +1,97 @@
+﻿//=============================================================================
+//
+// SHICHUANG CONFIDENTIAL
+// __________________
+//
+//  [2016] - [2018] SHICHUANG Co., Ltd.
+//  All Rights Reserved.
+//
+//=============================================================================
+
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Module.Controls
+{
+    /// <summary>
+    /// Interaction logic for EditableTextBoxWithRulexaml.xaml
+    /// </summary>
+    public partial class TextBoxWithRule : UserControl
+    {
+        #region Fields
+
+        public static readonly DependencyProperty ValidationParamProperty =
+     DependencyProperty.Register("ValidationParam", typeof(ValidationParams), typeof(TextBoxWithRule));
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register(
+            "Text",
+            typeof(string),
+            typeof(TextBoxWithRule),
+            new FrameworkPropertyMetadata("", FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+  
+        public static readonly DependencyProperty IsReadOnlyProperty =
+            DependencyProperty.Register(
+            "IsReadOnly",
+            typeof(bool),
+            typeof(TextBoxWithRule),
+            new PropertyMetadata(false));
+
+
+        #endregion Fields
+
+
+
+        #region Constructors
+
+        public TextBoxWithRule()
+        {
+            InitializeComponent();
+            base.Focusable = true;
+            base.FocusVisualStyle = null;
+            this.KeyDown += TextBoxWithRule_KeyDown;
+
+        }
+
+        private void TextBoxWithRule_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                TraversalRequest request = new TraversalRequest(FocusNavigationDirection.Next);
+                UIElement focusElement = Keyboard.FocusedElement as UIElement;
+                if (focusElement != null)
+                {
+                    focusElement.MoveFocus(request);
+                }
+                e.Handled = true;
+            }
+        }
+
+        #endregion Constructors       
+        #region Properties
+
+        public ValidationParams ValidationParam
+        {
+            get { return (ValidationParams)GetValue(ValidationParamProperty); }
+            set { SetValue(ValidationParamProperty, value); }
+        }
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return (bool)GetValue(IsReadOnlyProperty); }
+            set { SetValue(IsReadOnlyProperty, value); }
+
+        }
+
+        #endregion Properties
+    }
+}
---
+++ Module.Controls/ValidationParams.cs
@@ -0,0 +1,100 @@
+﻿//=============================================================================
+//
+// SHICHUANG CONFIDENTIAL
+// __________________
+//
+//  [2016] - [2018] SHICHUANG Co., Ltd.
+//  All Rights Reserved.
+//
+//=============================================================================
+
+using System;
+using System.Windows;
+
+namespace Module.Controls
+{
+    public class BindingProxy : Freezable
+    {
+        #region Fields
+
+        public static readonly DependencyProperty ValidationParamProperty =
+            DependencyProperty.Register("ValidationParam", typeof(ValidationParams), typeof(BindingProxy), new PropertyMetadata(null));
+
+        #endregion Fields
+
+        #region Properties
+
+        public ValidationParams ValidationParam
+        {
+            get { return (ValidationParams)GetValue(ValidationParamProperty); }
+            set { SetValue(ValidationParamProperty, value); }
+        }
+
+        #endregion Properties
+
+        #region Methods
+
+        protected override Freezable CreateInstanceCore()
+        {
+            return new BindingProxy();
+        }
+
+        #endregion Methods
+    }
+
+    public class ValidationParams : DependencyObject
+    {
+        #region Fields
+
+        public static readonly DependencyProperty IsAllowEmptyProperty =
+    DependencyProperty.Register("IsAllowEmpty", typeof(bool), typeof(ValidationParams), new PropertyMetadata(false));
+
+        public static readonly DependencyProperty MinValueProperty =
+    DependencyProperty.Register("MinValue", typeof(string), typeof(ValidationParams), new PropertyMetadata(""));
+
+        public static readonly DependencyProperty MaxValueProperty =
+    DependencyProperty.Register("MaxValue", typeof(string), typeof(ValidationParams), new PropertyMetadata(""));
+
+        public static readonly DependencyProperty ValuePatternProperty =
+    DependencyProperty.Register("ValuePattern", typeof(string), typeof(ValidationParams), new PropertyMetadata(""));
+
+        public static readonly DependencyProperty DataTypeProperty =
+    DependencyProperty.Register("DataType", typeof(string), typeof(ValidationParams), new PropertyMetadata("string"));
+
+        #endregion Fields
+
+        #region Properties
+
+        public bool IsAllowEmpty
+        {
+            get { return (bool)GetValue(IsAllowEmptyProperty); }
+            set { SetValue(IsAllowEmptyProperty, value); }
+        }
+
+        public string MinValue
+        {
+            get { return (string)GetValue(MinValueProperty); }
+            set { SetValue(MinValueProperty, value); }
+        }
+
+        public string MaxValue
+        {
+            get { return (string)GetValue(MaxValueProperty); }
+            set { SetValue(MaxValueProperty, value); }
+        }
+
+        public string ValuePattern
+        {
+            get { return (string)GetValue(ValuePatternProperty); }
+            set { SetValue(ValuePatternProperty, value); }
+        }
+
+        public string DataType
+        {
+            get { return (string)GetValue(DataTypeProperty); }
+            set { SetValue(DataTypeProperty, value); }
+        }
+
+        #endregion Properties
+    }
+}
